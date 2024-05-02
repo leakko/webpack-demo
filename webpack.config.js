5037f4363cedc2767757e6d9c3c574108c80e42d
Marcos Salvo <marcossalvo95@gmail.com>
2024-05-02 11:41:01 +0200

    Add development server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 const { mode } = require('webpack-nano/argv');
-
+const { WebpackPluginServe } = require("webpack-plugin-serve");
 const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin');
 
 module.exports = {
+    watch: mode === 'development',
+    entry: ['./src', 'webpack-plugin-serve/client'],
     mode,
     plugins: [
         new MiniHtmlWebpackPlugin({
             context: {
                 title: "Demo"
             }
+        }),
+        new WebpackPluginServe({
+            port: parseInt(process.env.PORT) || 8080,
+            static: './dist',
+            liveReload: true,
+            waitForBuild: true,
+            host: '127.0.0.1'
         })
-    ]
+    ],
+    target: 'web',
 }
